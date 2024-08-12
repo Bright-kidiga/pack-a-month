afc6355585f0a5da4574b346462760bb5e01c8ea
Muloma <KEN22954@kcbgroup.com>
2024-08-12 16:37:23 +0300

    backend commit

---
+++ Backend/pack-a-month/src/main/java/com/pam/packamonth/PackAMonthApplication.java
@@ -0,0 +1,13 @@
+package com.pam.packamonth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackAMonthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackAMonthApplication.class, args);
+    }
+
+}
---
+++ Backend/pack-a-month/src/test/java/com/pam/packamonth/PackAMonthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pam.packamonth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackAMonthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
